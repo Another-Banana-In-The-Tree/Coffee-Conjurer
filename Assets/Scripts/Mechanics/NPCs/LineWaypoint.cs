d90cf6495fb668d9050ac1a0bcafeb232775b8a2
Ryan <ryan.dean@ontariotechu.net>
2024-02-12 09:54:21 -0500

    Updated NPC system
    - Added moving between waypoints
    - NPCs set if they're interactive depending on which waypoint they're on
    - Lerp my beloved

--- Assets/Scripts/Mechanics/NPCs/LineWaypoint.cs
+++ Assets/Scripts/Mechanics/NPCs/LineWaypoint.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class LineWaypoint : MonoBehaviour
 {
-    List<NPCManager> npc;
+    List<NPC> npc;
+    
+    //Return the number of NPCs in line at this waypoint
     public int GetNPCCount()
     {
         return npc.Count;
+    }
+
+    //Return the position of the GameObject
+    public Vector3 GetPosition()
+    {
+        return gameObject.transform.position;
+    }
+
+    public void AddCustomer(NPC customer)
+    {
+        this.npc.Add(customer);
     }
 }
