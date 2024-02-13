3b4e238c69a7a9e0589e186f27e317e643cb2f10
Ryan <ryan.dean@ontariotechu.net>
2024-02-13 16:53:03 -0500

    Added NPC emote system / Changed NPC system
    - NPC system now functions mainly through the NPC script, with the NPCManager simply holding the waypoints and calling the move function
    
    - NPC emote system now in place with functioning emotes (May make it a function instead and set up image definition for the emotes in the NPCManager)

--- Assets/Scripts/Managers/NPCManager.cs
+++ Assets/Scripts/Managers/NPCManager.cs
@@ -8,35 +8,32 @@ using UnityEngine;
 public class NPCManager : MonoBehaviour
 {
     [SerializeField] List<NPC> _customers = new List<NPC>();
-    [SerializeField] LineWaypoint _entranceWaypoint, _orderWaypoint, _inBetweenWaypoint, _pickupWaypoint;
+    [SerializeField] public LineWaypoint _entranceWaypoint, _orderWaypoint, _inBetweenWaypoint, _pickupWaypoint;
     List<LineWaypoint> waypoints;
-    LineWaypoint currentWaypoint;
-    int _nextWaypointCounter = 0;
     int _inStoreCustomers;
     private void Awake()
     {
         //Load waypoints into list for ease of use
         waypoints = new List<LineWaypoint> { _entranceWaypoint, _orderWaypoint, _inBetweenWaypoint, _pickupWaypoint };
-
-        //Set the next waypoint to the nextWaypointCounter, which starts with first waypoint in the list
-        currentWaypoint = waypoints[_nextWaypointCounter];
     }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            //Move NPC to the next waypoint, increase the _nextWaypointCounter and set the new currentWaypoint
+            //Move NPC to the next waypoint, increase the _nextWaypointCounter, set the new currentWaypoint and set the next waypoint
             Debug.Log("Moving NPC!");
-            StartCoroutine(_customers[0].MoveToWaypoint(currentWaypoint));
-            if (_nextWaypointCounter < waypoints.Count - 1) _nextWaypointCounter += 1;
-            currentWaypoint = waypoints[_nextWaypointCounter];
-
-            //If the waypoint is the order waypoint or the pickup waypoint, set customer to Interactable, otherwise make them uninteractable
-            if (waypoints[_nextWaypointCounter + 1] == _orderWaypoint || waypoints[_nextWaypointCounter + 1] == _pickupWaypoint)
-            {
-                _customers[0].SetInteractable(true);
-            }
-            else _customers[0].SetInteractable(false);
+            StartCoroutine(_customers[0].MoveToWaypoint(_customers[0].GetNextWaypoint()));
         }
+
+        //If the waypoint is the order waypoint or the pickup waypoint, set customer to Interactable, otherwise make them uninteractable
+        if (_customers[0].GetCurrentWaypoint() == _orderWaypoint || _customers[0].GetCurrentWaypoint() == _pickupWaypoint)
+        {
+            _customers[0].SetInteractable(true);
+        }
+        else _customers[0].SetInteractable(false);
+    }
+    public List<LineWaypoint> GetWaypoints()
+    {
+        return waypoints;
     }
 }
